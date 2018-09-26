7f07faf41c6c052fdd9bca7d2bca235c012beacc
Nick Pfisterer <nick@polymoon.net>
2018-09-26 16:07:56 -0700

    Add Sass & PostCSS task

--- _gulp/gulpfile.babel.js
+++ _gulp/gulpfile.babel.js
@@ -1,9 +1,20 @@
 'use strict'
 
 import gulp from 'gulp'
+import sass from 'gulp-sass'
+import postcss from 'gulp-postcss'
+import autoprefixer from 'autoprefixer'
 
 // Sass -> PostCSS -> CSS
-function styles () {}
+function styles () {
+  // PostCSS plugins
+  const plugins = [ autoprefixer() ]
+
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('assets/css'))
+}
 
 // BrowserSync
 function server () {}
