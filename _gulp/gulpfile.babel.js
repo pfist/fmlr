d24b177f0eeb3b7974e15165858f415b534eb1e4
Nick Pfisterer <nick@polymoon.net>
2018-09-26 17:42:14 -0700

    Several minor fixes in gulpfile
    - Update paths  with to correctly go one directory up from _gulp
    - Add missing imports
    - Add 'gulp.' to gulp function calls
    - Add async completion signal to scan function
    - Add todo notes so I know where I left off

--- _gulp/gulpfile.babel.js
+++ _gulp/gulpfile.babel.js
@@ -5,7 +5,9 @@ import sass from 'gulp-sass'
 import postcss from 'gulp-postcss'
 import autoprefixer from 'autoprefixer'
 import browserSync from 'browser-sync'
-import pkg from './package.json'
+import pkg from '../package.json'
+import zip from 'gulp-zip'
+import gscan from 'gscan'
 
 const server = browserSync.create()
 
@@ -14,10 +16,10 @@ function styles () {
   // PostCSS plugins
   const plugins = [ autoprefixer() ]
 
-  return gulp.src('assets/sass/**/*.scss')
+  return gulp.src('../assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('../assets/css'))
     .pipe(server.stream())
 }
 
@@ -27,28 +29,31 @@ function preview () {
     proxy: 'http://localhost:2368'
   })
 
-  watch(['*.hbs', 'partials/*.hbs'], server.reload())
-  watch('assets/sass/**/*.scss', styles)
+  gulp.watch(['../*.hbs', '../partials/*.hbs'], server.reload)
+  gulp.watch('../assets/sass/**/*.scss', styles)
 }
 
 // Zip theme for production
-function zip () {
-  return gulp.src(['**/*', '!node_modules/**', `!${pkg.name}.zip`, '!assets/sass/**/*'])
+// TODO: Fix the file glob in gulp.src so it gets the correct files and puts them in the correct places
+function pack () {
+  return gulp.src(['../**/*.hbs', '../assets/**/**', '!../assets/**/*.scss', '../package.json'])
     .pipe(zip(`${pkg.name}.zip`))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('..'))
 }
 
 // Gscan
-function scan () {
+function scan (done) {
   gscan.checkZip({
-    path: `${pkg.name}.zip`,
+    path: `../${pkg.name}.zip`,
     name: pkg.name
   }).then(result => {
     console.log(result)
   }).catch(error => {
     console.error(error)
   })
+
+  done()
 }
 
-exports.develop = series(styles, preview)
-exports.pack = series(zip, scan)
+exports.develop = gulp.series(styles, preview)
+exports.pack = gulp.series(pack, scan)
