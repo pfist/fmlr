c7a036d873655bbdf0a6096d8dd82b3824a52cbe
Nick Pfisterer <nick@polymoon.net>
2018-11-05 11:04:02 -0800

    Switch to Listr for managing command tasks

--- src/commands/create.js
+++ src/commands/create.js
@@ -2,50 +2,54 @@
 
 const { Command, flags } = require('@oclif/command')
 const { cli } = require('cli-ux')
-// const fs = require('fs-extra')
+const Listr = require('listr')
+const fs = require('fs-extra')
 
 class CreateCommand extends Command {
   async run () {
-    // Prompt the user for information
+    // Prompt user for basic theme info
     const name = await cli.prompt('Theme name')
     const description = await cli.prompt('Theme description')
-    const license = await cli.prompt('License')
     const postsPerPage = await cli.prompt('Posts per page')
+
+    // Location where new theme will be created
     const themeDir = `${process.cwd()}/${name}`
 
-    // Scaffold a new theme
-    try {
-      cli.action.start('Creating theme files')
-
-      await fs.copy(`${__dirname}/../themes/starter`, themeDir)
-
-      cli.action.stop('Done!')
-    } catch (err) {
-      this.error(err)
-    }
-
-    // Create a package.json
-    try {
-      cli.action.start('Creating package.json')
-
-      const themePkg = {
-        name: name,
-        description: description,
-        version: '0.0.0',
-        engines: {
-          ghost: '>=2.0.0'
-        },
-        config: {
-          posts_per_page: postsPerPage
-        }
+    // Theme's package.json will be generated from this object
+    const themePkg = {
+      name: name,
+      description: description,
+      version: '0.0.0',
+      engines: {
+        ghost: '>=2.0.0'
+      },
+      config: {
+        posts_per_page: postsPerPage
       }
-
-      await fs.writeJson(`${themeDir}/package.json`, themePkg, { spaces: 2 })
-
-      cli.action.stop('Done!')
-    } catch (err) {
-      this.error(err)
     }
+
+    const tasks = new Listr([
+      // Scaffold a new theme
+      {
+        title: 'Creating theme files',
+        task: () => fs.copy(`${__dirname}/../themes/starter`, themeDir)
+        .catch(error => {
+          this.error(error)
+        })
+      },
+      // Generate a package.json
+      {
+        title: 'Creating package.json',
+        task: () => fs.writeJson(`${themeDir}/package.json`, themePkg, { spaces: 2 })
+        .catch(error => {
+          this.error(error)
+        })
+      }
+    ])
+
+    tasks.run().catch(error => {
+      this.error(error)
+    })
   }
 }
 
