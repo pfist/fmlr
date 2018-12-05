d11a6aa052fba51d849063a2257142167f143085
Nick Pfisterer <nick@polymoon.net>
2018-12-05 13:37:13 -0800

    Add "develop" command

--- src/commands/develop.js
+++ src/commands/develop.js
@@ -1,11 +1,73 @@
 const { Command, flags } = require('@oclif/command')
+const bs = require('browser-sync').create()
+const chokidar = require('chokidar')
+const fs = require('fs-extra')
+const Listr = require('listr')
+const { src, dest } = require('gulp')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const sass = require('gulp-sass')
+
+// fmlr config
+const config = require('../config')
+
+// Process assets with gulp
+function processAssets () {
+  return src(config.sass.src)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss([autoprefixer()]))
+  .pipe(dest(config.sass.dest))
+  .pipe(bs.stream())
+}
+
+// Watch theme files for changes
+function watchFiles () {
+  chokidar.watch(config.sass.src)
+  .on('change', path => processAssets())
+
+  chokidar.watch(config.templates.src)
+  .on('change', path => bs.reload())
+}
 
 class DevelopCommand extends Command {
   async run () {
-    // Make sure this directory is a Ghost theme
-    // Compile assets
-    // Start up BrowserSync
-    this.log('Hello from the Develop command!')
+    const tasks = new Listr([
+      // Make sure this is a Ghost theme
+      {
+        title: 'Checking theme',
+        task: (ctx, task) => fs.readJson('./package.json')
+        .then(pkg => {
+          if (pkg.engines.ghost) {
+            task.title = `Found ${pkg.name} ${pkg.version}`
+          } else {
+            this.error('No Ghost theme found')
+          }
+        })
+        .catch(error => {
+          this.error(error)
+        })
+      },
+      // Process assets
+      {
+        title: 'Processing assets',
+        task: () => processAssets()
+      },
+      // Start BrowserSync
+      {
+        title: 'Starting BrowserSync',
+        task: () => bs.init(config.browsersync)
+      },
+      // Watch theme files
+      {
+        title: 'Watching theme files',
+        task: () => watchFiles()
+      }
+    ])
+
+    tasks.run()
+    .catch(error => {
+      this.error(error)
+    })
   }
 }
 
