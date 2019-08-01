4cdeaec25a4d0f91d588017e121c30ecd6154e1c
Nick Pfisterer <nick@polymoon.net>
2019-07-31 18:12:27 -0700

    Rewrite Create command
    I removed listr entirely, and instead I'm handling the package.json updates inside the callback for download-git-repo. The nice UI provided by listr is lost, but this is simpler, less error prone, and honestly, listr didn't seem to be working properly anyways.

--- src/commands/create.js
+++ src/commands/create.js
@@ -2,53 +2,45 @@
 
 const { Command } = require('@oclif/command')
 const { cli } = require('cli-ux')
+const download = require('download-git-repo')
 const fs = require('fs-extra')
-const Listr = require('listr')
+
+// fmlr config
+const config = require('../fmlr.config.js')
 
 class CreateCommand extends Command {
   async run () {
     // Prompt user for basic theme info
     const name = await cli.prompt('Theme name')
     const description = await cli.prompt('Theme description')
-    const postsPerPage = await cli.prompt('Posts per page')
 
     // Location where new theme will be created
     const themeDir = `${process.cwd()}/${name}`
 
-    // Theme's package.json will be generated from this object
-    const themePkg = {
-      name: name,
-      description: description,
-      version: '0.0.0',
-      engines: {
-        ghost: '>=2.0.0'
-      },
-      config: {
-        posts_per_page: postsPerPage
+    // Download default theme
+    download(config.defaultTheme, themeDir, err => {
+      if (err) {
+        this.error(err)
       }
-    }
 
-    const tasks = new Listr([
-      // Scaffold a new theme
-      {
-        title: 'Creating theme files',
-        task: () => fs.copy(`${__dirname}/../themes/starter`, themeDir)
-        .catch(error => {
-          this.error(error)
-        })
-      },
-      // Generate a package.json
-      {
-        title: 'Creating package.json',
-        task: () => fs.writeJson(`${themeDir}/package.json`, themePkg, { spaces: 2 })
-        .catch(error => {
-          this.error(error)
-        })
-      }
-    ])
+      // Get theme's package.json
+      fs.readJson(`${themeDir}/package.json`)
+        .then(pkg => {
+          const themeInfo = {
+            name: name,
+            description: description,
+            version: '0.0.0'
+          }
 
-    tasks.run().catch(error => {
-      this.error(error)
+          // Overwrite name and description
+          fs.writeJson(`${themeDir}/package.json`, { ...pkg, ...themeInfo }, { spaces: 2 }, err => {
+            if (err) {
+              this.error(err)
+            }
+
+            console.log('Your theme is ready!')
+          })
+        })
     })
   }
 }
