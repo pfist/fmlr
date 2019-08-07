79d44b2362d6ef68d188fee201629b85d3e83df4
Nick Pfisterer <nick@polymoon.net>
2019-08-06 17:07:55 -0700

    Refactor config file

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  defaultTheme: 'polymoon/spirit',
+  options: {
+    sass: {
+      style: 'compressed'
+    },
+    browserSync: {
+      logPrefix: 'fmlr',
+      proxy: 'localhost:2368'
+    }
+  }
+}
