53b9928fee56349978aa08d2329492ee3e4ba96f
Nick Pfisterer <nick@polymoon.net>
2019-08-06 19:58:45 -0700

    Starting on Dev command

--- src/commands/dev.js
+++ src/commands/dev.js
@@ -1,9 +1,22 @@
 const { Command } = require('@oclif/command')
 const { cli } = require('cli-ux')
+const config = require('../config')
+const fs = require('fs-extra')
+const tasks = require('../tasks')
 
 class DevCommand extends Command {
   async run () {
-    // Add command logic here
+    try {
+      // Make sure there's a valid Ghost theme in this directory
+      const pkg = await fs.readJson('./package.json')
+      
+      cli.action.start(`Found ${pkg.name} ${pkg.version}. Starting development server`)
+
+      // Start development server
+      tasks.dev()
+    } catch (err) {
+      this.log('No Ghost theme found.')
+    }
   }
 }
 
